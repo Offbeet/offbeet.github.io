4e0a95a949e783eaa7801e87cd722aac80d864d0
Offbeet <just6070just6070@gmail.com>
2025-04-14 17:13:24 -0400

    Home Button Hotfix
    Fixed a bug preventing you from returning to the home menu. Oops.

--- js/top-bar.js
+++ js/top-bar.js
@@ -1,7 +1,7 @@
 document.getElementById("main").innerHTML = `
 <div class="off-screen-menu" style="z-index: 2;">
   <ul>
-    <li><a href="./">Home</a></li>
+    <li><a href="../">Home</a></li>
     <li><a href="/About-Me/">About Me</a></li>
     <li><a href="/Portfolio/">Portfolio</a></li>
     <li><a href="/Links/">Links</a></li>
